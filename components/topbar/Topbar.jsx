e8857d9e56268d65f0b56076dcf616444c5a34e4
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-01-11 19:01:45 +0100

    Passed navigationLinks to Topbar

--- components/topbar/Topbar.jsx
+++ components/topbar/Topbar.jsx
@@ -1,11 +1,16 @@
 import styles from './Topbar.module.scss';
 
-export default function Topbar({}) {
+export default function Topbar({navigationLinks}) {
 	return (
 		<nav className="topbar">
 			<div className="topbar__left">
 			</div>
 			<div className="topbar__right">
+			{
+				navigationLinks.map((link) =>{
+					return link.name;
+				})
+			}
 			</div>
 		</nav>
 	)
