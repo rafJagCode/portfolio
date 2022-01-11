a4fa7f0509c3b3fd96180d85175b4f7c2d6d6108
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-01-11 17:47:33 +0100

    Added topbar component

---
+++ components/topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import styles from './Topbar.module.scss';
+
+export default function Topbar({}) {
+	return (
+		<nav className="topbar">
+			<div className="topbar__left">
+			</div>
+			<div className="topbar__right">
+			</div>
+		</nav>
+	)
+}
