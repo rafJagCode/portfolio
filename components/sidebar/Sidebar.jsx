08f410f9ed9c04c6f4627c770ae546fd98e56052
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-01-11 18:47:39 +0100

    Fixed wrong file name

--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -1,10 +1,10 @@
-import './Sidebar.module.scss';
+import styles from './Sidebar.module.scss';
 
 export default function Sidebar() {
 	return (
 		<nav className="sidebar">
-		<div className="sidebar__links">
-		</div>
+			<div className="sidebar__links">
+			</div>
 		</nav>
 	)
 }
