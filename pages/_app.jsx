d13ce89f5ee4cad948060bd402c861fbf542e9ff
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-01-13 02:39:53 +0100

    Started adding page loader

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,19 @@
 import './scss/reset.scss';
 import './scss/global.scss';
 import './scss/themes.scss';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+	const [pageLoading, setPageLoading] = useState(true);
 
 	useEffect(()=>{
 		document.querySelector('html').lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'pl';
+		setPageLoading(false);
 	}, []);
 
   return (
+	  pageLoading ?
+	  <div>Loading</div> :
       <Component {...pageProps} />
   )
 }
