f0049f6e4020c15cf9d04f33c53cd8d66b56f4da
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-01-16 22:27:56 +0100

    Created home section

--- components/sections/Sections.jsx
+++ components/sections/Sections.jsx
@@ -1,6 +1,7 @@
 import ParticlesBackground from '@/components/particles_background/ParticlesBackground';
 import ReactFullpage from '@fullpage/react-fullpage';
 import styles from './Sections.module.scss';
+import Home from '@/components/home/Home';
 import Footer from '@/components/footer/Footer';
 import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
@@ -20,6 +21,7 @@ export default function Sections({state}){
 		<ReactFullpage.Wrapper key="fullpage__wrapper">
 			<section className="section">
 				<div className={styles.section__container}>
+					<Home/>
 				</div>
 			</section>
 			<section className="section">
