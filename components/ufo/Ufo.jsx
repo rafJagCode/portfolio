14a7c5328dc340c0281d5d8f189106133b97ddd4
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-09-01 00:20:32 +0200

    Fixed ufoComponent positioning after resize

--- components/ufo/Ufo.jsx
+++ components/ufo/Ufo.jsx
@@ -1,15 +1,23 @@
 import styles from "./Ufo.module.scss";
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 export default function Ufo({ isVisible }) {
+  const fullpageApi = useSelector((state) => state.fullpageApi);
   const ufoComponent = useRef();
 
   const ufoContainerObserver = new ResizeObserver((entries) => {
     const ufoContainer = entries[0];
     const ufoContainerBoundingClientRect = ufoContainer.target.getBoundingClientRect();
     const ufoWidth = ufoContainerBoundingClientRect.width + "px";
-    const topInPercents = (parseFloat(ufoContainerBoundingClientRect.top) / window.innerHeight) * 100;
     const leftInPercents = (parseFloat(ufoContainerBoundingClientRect.left) / window.innerWidth) * 100;
+    const activeSectionIndex = fullpageApi.getActiveSection().index;
+    let topInPercents;
+    if (!activeSectionIndex) {
+      topInPercents = (parseFloat(ufoContainerBoundingClientRect.top) / window.innerHeight) * 100;
+    } else {
+      topInPercents = ((activeSectionIndex * window.innerHeight + parseFloat(ufoContainerBoundingClientRect.top)) / window.innerHeight) * 100;
+    }
 
     ufoComponent.current.style.width = ufoWidth;
     ufoComponent.current.style.top = topInPercents + "%";
