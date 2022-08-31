521eb82461a2b1efcc38f6be1d3de0fdb381f3da
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-08-31 03:40:54 +0200

    Added ufo component resizing

--- components/ufo/Ufo.jsx
+++ components/ufo/Ufo.jsx
@@ -1,11 +1,34 @@
-import styles from './Ufo.module.scss';
-import {useSelector} from 'react-redux';
-import {useEffect} from 'react';
+import styles from "./Ufo.module.scss";
+import { useEffect, useRef } from "react";
 
-export default function Ufo() {
+export default function Ufo({ isVisible }) {
+  const ufoComponent = useRef();
+
+  const ufoContainerObserver = new ResizeObserver((entries) => {
+    const fireSizeFactor = 1 + 0.6;
+    const ufoContainer = entries[0];
+    const ufoContainerBoundingClientRect = ufoContainer.target.getBoundingClientRect();
+    const ufoWidth = ufoContainerBoundingClientRect.width + "px";
+    const ufoHeight = ufoContainerBoundingClientRect.height * fireSizeFactor + "px";
+    const topInPercents = (parseFloat(ufoContainerBoundingClientRect.top) / window.innerHeight) * 100;
+    const leftInPercents = (parseFloat(ufoContainerBoundingClientRect.left) / window.innerWidth) * 100;
+    ufoComponent.current.style.top = topInPercents + "%";
+    ufoComponent.current.style.left = leftInPercents + "%";
+
+    ufoComponent.current.style.width = ufoWidth;
+    ufoComponent.current.style.height = ufoHeight;
+  });
+
+  useEffect(() => {
+    const ufoContainer = document.getElementById("home__ufo_container");
+    if (isVisible) ufoContainerObserver.observe(ufoContainer);
+    else ufoContainerObserver.unobserve(ufoContainer);
+  }, [isVisible]);
+
   return (
-	<div className={styles.ufo} id='ufo'>
-		<div className={styles.ufo__image}></div>
-		<div className={styles.ufo__fire} id='ufo__fire'></div>
-	</div>);
+    <div className={styles.ufo} id="ufo" ref={ufoComponent}>
+      <div className={styles.ufo__image}></div>
+      <div className={styles.ufo__fire} id="ufo__fire"></div>
+    </div>
+  );
 }
