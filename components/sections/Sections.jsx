c48f7c8566090ed11b12059d9ea7dbaa169fcad6
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-08-31 15:49:59 +0200

    Added scrolling on hashchange

--- components/sections/Sections.jsx
+++ components/sections/Sections.jsx
@@ -1,42 +1,45 @@
-import ParticlesBackground from '@/components/particles_background/ParticlesBackground';
-import ReactFullpage from '@fullpage/react-fullpage';
-import styles from './Sections.module.scss';
-import Home from '@/components/home/Home';
-import Projects from '@/components/projects/Projects';
-import Footer from '@/components/footer/Footer';
-import {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import ParticlesBackground from "@/components/particles_background/ParticlesBackground";
+import ReactFullpage from "@fullpage/react-fullpage";
+import styles from "./Sections.module.scss";
+import Home from "@/components/home/Home";
+import Projects from "@/components/projects/Projects";
+import Footer from "@/components/footer/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-export default function Sections({state}){
-	const dispatch = useDispatch();
+export default function Sections({ state }) {
+  const dispatch = useDispatch();
+  window.onhashchange = (e) => {
+    state.fullpageApi.moveTo(`#${e.newURL.split("#")[1]}`);
+  };
 
-	useEffect(()=>{
-		const fullpageApi = state.fullpageApi;
-		dispatch({type: 'SET_FULLPAGE_API', fullpageApi: fullpageApi});
-		const anchor = '#' + window.location.hash.substr(1);
-		fullpageApi.moveTo(anchor);
-	}, []);
+  useEffect(() => {
+    const fullpageApi = state.fullpageApi;
+    dispatch({ type: "SET_FULLPAGE_API", fullpageApi: fullpageApi });
+    let anchor = "#" + window.location.hash.substr(1);
+    fullpageApi.moveTo(anchor);
+  }, []);
 
-	return([
-		<ParticlesBackground key="particles"/>,
-		<ReactFullpage.Wrapper key="fullpage__wrapper">
-			<section className="section">
-				<div className={styles.section__container}>
-					<Home/>
-				</div>
-			</section>
-			<section className="section">
-				<div className={styles.section__container}>
-					<Projects/>
-				</div>
-			</section>
-			<section className="section">
-				<div className={styles.section__container}></div>
-			</section>
-			<section className="section">
-				<div className={styles.section__container} data-is-last="true"></div>
-				<Footer/>
-			</section>
-		</ReactFullpage.Wrapper>
-	]);
+  return [
+    <ParticlesBackground key="particles" />,
+    <ReactFullpage.Wrapper key="fullpage__wrapper">
+      <section className="section">
+        <div className={styles.section__container}>
+          <Home />
+        </div>
+      </section>
+      <section className="section">
+        <div className={styles.section__container}>
+          <Projects />
+        </div>
+      </section>
+      <section className="section">
+        <div className={styles.section__container}></div>
+      </section>
+      <section className="section">
+        <div className={styles.section__container} data-is-last="true"></div>
+        <Footer />
+      </section>
+    </ReactFullpage.Wrapper>,
+  ];
 }
