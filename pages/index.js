a072c786d05ec66a60f47aec293be344d200b75c
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-08-31 03:40:14 +0200

    Beautified files

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,63 @@
-import styles from './App.module.scss';
-import Sidebar from '@/components/sidebar/Sidebar';
-import Topbar from '@/components/topbar/Topbar';
-import ReactFullpage from '@fullpage/react-fullpage';
-import Sections from '@/components/sections/Sections';
-import animateToLaunchingPosition from '@/services/ufo/animateToLaunchingPosition';
-import swapOrbitingUfoToUfoComponent from '@/services/ufo/swapOrbitingUfoToUfoComponent';
-import startUfoEngine from '@/services/ufo/startUfoEngine';
-import Ufo from '@/components/ufo/Ufo';
-import {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {defaultTranslation} from '@/translations/translations';
+import styles from "./App.module.scss";
+import Sidebar from "@/components/sidebar/Sidebar";
+import Topbar from "@/components/topbar/Topbar";
+import ReactFullpage from "@fullpage/react-fullpage";
+import Sections from "@/components/sections/Sections";
+import animateToLaunchingPosition from "@/services/ufo/animateToLaunchingPosition";
+import swapOrbitingUfoToUfoComponent from "@/services/ufo/swapOrbitingUfoToUfoComponent";
+import startUfoEngine from "@/services/ufo/startUfoEngine";
+import Ufo from "@/components/ufo/Ufo";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { defaultTranslation } from "@/translations/translations";
 
 export default function App() {
-	const sidebarOpen = useSelector(state => state.sidebarOpen);
-	const fullpageApi = useSelector(state => state.fullpageApi);
-	const dispatch = useDispatch();
+  const sidebarOpen = useSelector((state) => state.sidebarOpen);
+  const fullpageApi = useSelector((state) => state.fullpageApi);
+  const dispatch = useDispatch();
+  const [isUfoComponentVisible, setIsUfoComponentVisible] = useState(false);
 
-	useEffect(()=>{
-		const language = localStorage.getItem('language') ? localStorage.getItem('language') : defaultTranslation;
-		dispatch({type: 'CHANGE_LANGUAGE', language: language});
-	}, [])
+  useEffect(() => {
+    const language = localStorage.getItem("language") ? localStorage.getItem("language") : defaultTranslation;
+    dispatch({ type: "CHANGE_LANGUAGE", language: language });
+  }, []);
 
-	const onLeave = (origin, destination, direction) => {
-		dispatch({type: 'SET_SCROLLING_STATE', scrollState: {origin, destination, direction}});
-		const ufoOrbit = document.getElementById('home_image__orbit');
-		const ufoContainer = document.getElementById('home__ufo_container');
+  const onLeave = (origin, destination, direction) => {
+    dispatch({
+      type: "SET_SCROLLING_STATE",
+      scrollState: { origin, destination, direction },
+    });
+    const ufoOrbit = document.getElementById("home_image__orbit");
+    const ufoContainer = document.getElementById("home__ufo_container");
 
-		const ufoOrbitAnimation = ufoOrbit.getAnimations()[0];
-		const ufoContainerAnimation = ufoContainer.getAnimations()[0];
+    const ufoOrbitAnimation = ufoOrbit.getAnimations()[0];
+    const ufoContainerAnimation = ufoContainer.getAnimations()[0];
 
-		animateToLaunchingPosition(ufoOrbitAnimation, ufoContainerAnimation)
-		.then(()=>{
-			swapOrbitingUfoToUfoComponent(ufoContainer);
-			startUfoEngine();
-		})
+    animateToLaunchingPosition(ufoOrbitAnimation, ufoContainerAnimation).then(() => {
+      swapOrbitingUfoToUfoComponent(ufoContainer);
+      setIsUfoComponentVisible(true);
+      startUfoEngine();
+    });
 
-		return false;
-	}
+    return false;
+  };
 
-	return (
-		<div className={styles.app__container}>
-			<Sidebar/>
-			<Topbar/>
-			<main className={styles.app__content} data-is-blured={sidebarOpen}>
-				<ReactFullpage
-					licenseKey = {`Ca(6HeW5q[zX%k*>A#'V%I@@k|5*QKt)s]k3HmOaAPWk}sQdFK('[p0:wkl3f=|Fo0%ijhruscuZC\SYLWQI`}
-					scrollingSpeed = {1000}
-					dragAndMove = {true}
-					touchSensitivity = {3}
-					anchors = {['#home', '#projects', '#technologies', '#contact']}
-					onLeave = {onLeave}
-					render={(state)=><Sections state={state}/>}
-				/>
-				<Ufo/>
-			</main>
-		</div>
-	)
+  return (
+    <div className={styles.app__container}>
+      <Sidebar />
+      <Topbar />
+      <main className={styles.app__content} data-is-blured={sidebarOpen}>
+        <ReactFullpage
+          licenseKey={`Ca(6HeW5q[zX%k*>A#'V%I@@k|5*QKt)s]k3HmOaAPWk}sQdFK('[p0:wkl3f=|Fo0%ijhruscuZC\SYLWQI`}
+          scrollingSpeed={1000}
+          dragAndMove={true}
+          touchSensitivity={3}
+          anchors={["#home", "#projects", "#technologies", "#contact"]}
+          onLeave={onLeave}
+          render={(state) => <Sections state={state} />}
+        />
+        <Ufo isVisible={isUfoComponentVisible} />
+      </main>
+    </div>
+  );
 }
--- services/ufo/swapOrbitingUfoToUfoComponent.js
+++ services/ufo/swapOrbitingUfoToUfoComponent.js
@@ -1,12 +1,14 @@
 const swapOrbitingUfoToUfoComponent = (ufoContainer) => {
-	const fireSizeFactor = 1 + 0.6;
-	const ufoComponent = document.getElementById('ufo');
-	const ufoContainerBoundingClientRect  = ufoContainer.getBoundingClientRect();
-	ufoComponent.style.top = ufoContainerBoundingClientRect.top + 'px';
-	ufoComponent.style.left = ufoContainerBoundingClientRect.left + 'px';
-	ufoComponent.style.height = parseFloat(ufoContainerBoundingClientRect.height) * fireSizeFactor + 'px';
-	ufoComponent.style.width = ufoContainerBoundingClientRect.width + 'px';
-	ufoContainer.style.visibility = 'hidden';
-}
+  const fireSizeFactor = 1 + 0.6;
+  const ufoComponent = document.getElementById("ufo");
+  const ufoContainerBoundingClientRect = ufoContainer.getBoundingClientRect();
+  const topInPercents = (parseFloat(ufoContainerBoundingClientRect.top) / window.innerHeight) * 100;
+  const leftInPercents = (parseFloat(ufoContainerBoundingClientRect.left) / window.innerWidth) * 100;
+  ufoComponent.style.top = topInPercents + "%";
+  ufoComponent.style.left = leftInPercents + "%";
+  ufoComponent.style.height = parseFloat(ufoContainerBoundingClientRect.height) * fireSizeFactor + "px";
+  ufoComponent.style.width = ufoContainerBoundingClientRect.width + "px";
+  ufoContainer.style.visibility = "hidden";
+};
 
 export default swapOrbitingUfoToUfoComponent;
