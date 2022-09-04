876a2daf33ff5fb2bb952191be629fa5861527a0
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-09-05 00:10:20 +0200

    Added images  to projects section

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -1,9 +1,15 @@
-import styles from './Projects.module.scss';
+import styles from "./Projects.module.scss";
 
 export default function Projects() {
-	return (
-		<div className={styles.projects}>
-			<div className={styles.projects__container}>
+  return (
+    <div className={styles.projects}>
+      <div className={styles.projects__island_container}>
+        <div className={styles.projects__island}></div>
+        <div className={styles.cow1}></div>
+        <div className={styles.cow2}></div>
+        <div className={styles.cow3}></div>
+      </div>
+      {/* <div className={styles.projects__container}>
 				<div className={styles.projects__ufo_container} id='projects__ufo_container'></div>
 				<div className={styles.projects__plain}>
 					<div className={styles.projects__sun}></div>
@@ -17,7 +23,7 @@ export default function Projects() {
 						<div className={styles.projects__saturn_planet}></div>
 					</div>
 				</div>
-			</div>
-		</div>
-	)
+			</div> */}
+    </div>
+  );
 }
