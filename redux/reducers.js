bb9ef559cd2f1a59b8d580841c676e3079753775
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-09-05 02:42:58 +0200

    Added hovered cow state variable

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,56 +1,72 @@
-import types from './types'
-import {combineReducers} from 'redux';
+import types from './types';
+import { combineReducers } from 'redux';
 import store from './store';
 
 const INITIAL_LANGUAGE = 'pl';
 
 const language = (state = INITIAL_LANGUAGE, action) => {
-	switch(action.type){
-		case types.CHANGE_LANGUAGE:
-			localStorage.setItem('language', action.language);
-			document.documentElement.lang = action.language;
-			return action.language;
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case types.CHANGE_LANGUAGE:
+      localStorage.setItem('language', action.language);
+      document.documentElement.lang = action.language;
+      return action.language;
+    default:
+      return state;
+  }
+};
 
 const FULLPAGE_API = null;
 
 const fullpageApi = (state = FULLPAGE_API, action) => {
-	switch(action.type){
-		case types.SET_FULLPAGE_API:
-			return action.fullpageApi;
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case types.SET_FULLPAGE_API:
+      return action.fullpageApi;
+    default:
+      return state;
+  }
+};
 
 const SIDEBAR_OPEN = false;
 
 const sidebarOpen = (state = SIDEBAR_OPEN, action) => {
-	switch(action.type){
-		case types.CHANGE_SIDEBAR_STATE:
-			return !state;
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case types.CHANGE_SIDEBAR_STATE:
+      return !state;
+    default:
+      return state;
+  }
+};
 
 const SCROLL_STATE = {
-	origin: undefined,
-	destination: undefined,
-	direction: undefined,
-}
+  origin: undefined,
+  destination: undefined,
+  direction: undefined,
+};
 
-const scrollState = (state = SCROLL_STATE, action) =>{
-	switch(action.type){
-		case types.SET_SCROLLING_STATE:
-			return {...state, ...action.scrollState};
-		default: return state;
-	}
-}
+const scrollState = (state = SCROLL_STATE, action) => {
+  switch (action.type) {
+    case types.SET_SCROLLING_STATE:
+      return { ...state, ...action.scrollState };
+    default:
+      return state;
+  }
+};
+
+const HOVERED_COW = null;
+
+const hoveredCow = (state = HOVERED_COW, action) => {
+  switch (action.type) {
+    case types.SET_HOVERED_COW:
+      return action.cow;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
-    language, fullpageApi, sidebarOpen, scrollState
+  language,
+  fullpageApi,
+  sidebarOpen,
+  scrollState,
+  hoveredCow,
 });
