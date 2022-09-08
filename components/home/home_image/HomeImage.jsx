f38eba8147c0c10c9307393b94b61e8715232e3e
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-09-08 02:48:36 +0200

    Added dispatching home image ref

--- components/home/home_image/HomeImage.jsx
+++ components/home/home_image/HomeImage.jsx
@@ -1,5 +1,19 @@
 import styles from './HomeImage.module.scss';
+import { useRef, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function HomeImage() {
-  return <div>HomeImage</div>;
+  const earthRef = useRef();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'SET_GLOBAL_REFS', element: 'earth', ref: earthRef });
+  }, []);
+
+  return (
+    <div
+      className={styles.home__image}
+      ref={earthRef}
+    ></div>
+  );
 }
