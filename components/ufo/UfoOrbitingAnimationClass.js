1a7bb194e2625fa055aa01355fab6ebba5abd69a
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-09-12 04:55:12 +0200

    Refactored whole UfoEngineAnimationClass

--- components/ufo/UfoOrbitingAnimationClass.js
+++ components/ufo/UfoOrbitingAnimationClass.js
@@ -1,167 +1,145 @@
 class UfoOrbitingAnimationClass {
-  #start;
-  #ufoElement;
-  #earthElement;
-  #ufoTop;
-  #ufoLeft;
-  #ufoCenterTop;
-  #ufoCenterLeft;
-  #firstEarthCornerX;
-  #firstEarthCornerY;
-  #secondEarthCornerX;
-  #secondEarthCornerY;
-  #earthCenterLeft;
-  #orbitToRight;
+  #requestAnimationID;
+  #ufo;
+  #earth;
+  #stop;
   #speed;
-  #animation;
-  #isUfoLeavingHomeSection;
-  #animationPromise;
-  #animationPromiseResolve;
-  #goToLaunchingPositionIsOn;
+  #baseSpeed;
+  #destination;
+  #direction;
+  #depth;
+  #resolve;
 
-  constructor(ufoElement, earthElement) {
-    this.#start = undefined;
-    this.#ufoElement = ufoElement;
-    this.#earthElement = earthElement;
-    this.#ufoTop = undefined;
-    this.#ufoLeft = undefined;
-    this.#ufoCenterTop = undefined;
-    this.#ufoCenterLeft = undefined;
-    this.#firstEarthCornerX = undefined;
-    this.#firstEarthCornerY = undefined;
-    this.#secondEarthCornerX = undefined;
-    this.#secondEarthCornerY = undefined;
-    this.#earthCenterLeft = undefined;
-    this.#orbitToRight = true;
-    this.#speed = 3;
-    this.#animation = undefined;
-    this.#isUfoLeavingHomeSection = false;
-    this.#animationPromise = undefined;
-    this.#animationPromiseResolve = undefined;
-    this.#goToLaunchingPositionIsOn = false;
+  constructor(ufo, earth) {
+    this.#ufo = ufo;
+    this.#earth = earth;
+    this.reset();
   }
 
-  step = (timestamp) => {
-    this.setStartTime();
-    this.setUfoPosition();
-    this.setEarthCorners();
-    this.changeUfoSize();
-    if (this.stopAnimationIfConditionsFulliled()) return;
-    if (this.#orbitToRight) this.goToFirstEarthCorner();
-    if (!this.#orbitToRight) this.goToSecondEarthCorner();
-    this.#animation = requestAnimationFrame(this.step);
+  step = () => {
+    this.setDestination();
+    this.setDepth();
+    this.setSpeed();
+    let deltaX = this.getDeltaX();
+    let deltaY = this.getDeltaY();
+    let move = this.getMove(deltaX, deltaY);
+
+    if ((this.#direction === 'RIGHT' && deltaX <= this.#speed) || (this.#direction === 'LEFT' && deltaX >= -this.#speed)) this.changeDirection();
+    this.changeStyles(move);
+    this.#requestAnimationID = requestAnimationFrame(this.step);
+    if (this.shouldStop(deltaX)) this.stop();
   };
 
-  setStartTime = (timestamp) => {
-    if (this.#start) return;
-    this.#start = timestamp;
+  startOrbiting = () => {
+    if (this.#requestAnimationID) return;
+    this.#requestAnimationID = requestAnimationFrame(this.step);
   };
 
-  setUfoPosition = () => {
-    this.#ufoTop = this.#ufoElement.offsetTop;
-    this.#ufoLeft = this.#ufoElement.offsetLeft;
-    this.#ufoCenterTop = this.#ufoTop + this.#ufoElement.offsetHeight / 2;
-    this.#ufoCenterLeft = this.#ufoLeft + this.#ufoElement.offsetWidth / 2;
+  stopOrbiting = () => {
+    if (!this.#requestAnimationID) return new Promise(reject(new Error('There is nothing to stop')));
+    this.#stop = true;
+    this.#baseSpeed *= 4;
+    return new Promise((resolve) => (this.#resolve = resolve));
   };
 
-  setEarthCorners = () => {
-    this.#firstEarthCornerX = this.#earthElement.offsetLeft + this.#earthElement.offsetWidth;
-    this.#firstEarthCornerY = this.#earthElement.offsetTop;
-    this.#secondEarthCornerX = this.#earthElement.offsetLeft;
-    this.#secondEarthCornerY = this.#earthElement.offsetTop + this.#earthElement.offsetHeight;
-    this.#earthCenterLeft = this.#earthElement.offsetLeft + this.#earthElement.offsetWidth / 2;
+  reset = () => {
+    this.#requestAnimationID = undefined;
+    this.#stop = false;
+    this.#baseSpeed = 3;
+    this.#direction = 'RIGHT';
   };
 
-  goToFirstEarthCorner = () => {
-    let xDifference = this.#firstEarthCornerX - this.#ufoCenterLeft;
-    let yDifference = this.#firstEarthCornerY - this.#ufoCenterTop;
-    if (xDifference <= 0) {
-      this.#orbitToRight = false;
-      this.#ufoElement.style.zIndex = 0;
-      return;
+  changeDirection = () => {
+    this.#direction = this.#direction === 'RIGHT' ? 'LEFT' : 'RIGHT';
+  };
+
+  setDestination = () => {
+    let x;
+    let y;
+    switch (this.#direction) {
+      case 'RIGHT':
+        x = this.#earth.offsetLeft + this.#earth.offsetWidth;
+        y = this.#earth.offsetTop;
+        break;
+      case 'LEFT':
+        x = this.#earth.offsetLeft;
+        y = this.#earth.offsetTop + this.#earth.offsetHeight;
+        break;
     }
-    let derivative = yDifference / xDifference;
-    this.moveUfo(derivative, this.#speed);
+    this.#destination = {
+      x: x,
+      y: y,
+    };
   };
 
-  goToSecondEarthCorner = () => {
-    let xDifference = this.#secondEarthCornerX - this.#ufoCenterLeft;
-    let yDifference = this.#secondEarthCornerY - this.#ufoCenterTop;
-    if (xDifference >= 0) {
-      this.#orbitToRight = true;
-      this.#ufoElement.style.zIndex = 2;
-      return;
-    }
-    let derivative = yDifference / xDifference;
-    this.moveUfo(derivative, -this.#speed);
+  getMove = (deltaX, deltaY) => {
+    let derivative = deltaX === 0 ? 0 : deltaY / deltaX;
+    let y = this.#direction === 'RIGHT' ? derivative * this.#speed : derivative * -this.#speed;
+    return {
+      x: deltaX >= 0 ? this.#speed : -this.#speed,
+      y: y,
+    };
   };
 
-  moveUfo = (derivative, xMove) => {
-    let newTopPos = this.#ufoLeft + xMove;
-    let newLeftPos = this.#ufoTop + derivative * xMove;
-    this.#ufoElement.style.left = `${newTopPos}px`;
-    this.#ufoElement.style.top = `${newLeftPos}px`;
+  changeStyles = (move) => {
+    let zIndex = this.#direction === 'RIGHT' ? 2 : 0;
+    let scale = this.getScale();
+    this.#ufo.style.left = `${this.#ufo.offsetLeft + move.x}px`;
+    this.#ufo.style.top = `${this.#ufo.offsetTop + move.y}px`;
+    this.#ufo.style.zIndex = zIndex;
+    this.#ufo.style.transform = `scale(${scale})`;
   };
 
-  changeUfoSize = () => {
-    let isUfoFlyingAway = this.#ufoCenterLeft >= this.#earthCenterLeft;
-    if (isUfoFlyingAway) this.reduceUfoSize();
-    if (!isUfoFlyingAway) this.increaseUfoSize();
+  getDeltaX = () => {
+    return this.#destination.x - this.#ufo.offsetLeft - this.#ufo.offsetWidth / 2;
   };
 
-  reduceUfoSize = () => {
-    let distance;
-    if (this.#orbitToRight) distance = this.#ufoCenterLeft - this.#earthCenterLeft;
-    if (!this.#orbitToRight) distance = this.#earthElement.offsetWidth - (this.#ufoCenterLeft - this.#earthCenterLeft);
-    let sizeFactor = 0.2;
-    let scale = 1 - (distance / (this.#earthElement.offsetWidth / 2)) * sizeFactor;
-    this.#ufoElement.style.transform = `scale(${scale})`;
+  getDeltaY = () => {
+    return this.#destination.y - this.#ufo.offsetTop - this.#ufo.offsetHeight / 2;
   };
 
-  increaseUfoSize = () => {
-    let distance;
-    if (!this.#orbitToRight) distance = this.#earthElement.offsetWidth - (this.#earthCenterLeft - this.#ufoCenterLeft);
-    if (this.#orbitToRight) distance = this.#earthCenterLeft - this.#ufoCenterLeft;
-    let sizeFactor = 0.2;
-    let scale = 1 - (distance / (this.#earthElement.offsetWidth / 2)) * sizeFactor;
-    this.#ufoElement.style.transform = `scale(${scale})`;
+  setDepth = () => {
+    let quarterOrbitLength = this.#earth.offsetWidth / 2;
+    let distance = this.getDistanceFromEarthCenter();
+    let currentQuarter = this.getCurrentOrbitQuarterIndex(distance);
+    let depth;
+    if (currentQuarter === 1) depth = distance;
+    if (currentQuarter === 2) depth = 2 * quarterOrbitLength - distance;
+    if (currentQuarter === 3) depth = 2 * quarterOrbitLength + distance;
+    if (currentQuarter === 4) depth = -distance;
+    this.#depth = depth / (2 * quarterOrbitLength);
   };
 
-  animate = () => {
-    if (this.#animation) return;
-    this.#animationPromise = new Promise((resolve) => (this.#animationPromiseResolve = resolve));
-    this.#animation = requestAnimationFrame(this.step);
+  getDistanceFromEarthCenter = () => {
+    return this.#ufo.offsetLeft + this.#ufo.offsetWidth / 2 - (this.#earth.offsetLeft + this.#earth.offsetWidth / 2);
   };
 
-  goToLaunchingPosition = () => {
-    if (this.#goToLaunchingPositionIsOn) return;
-    this.#goToLaunchingPositionIsOn = true;
-    this.#speed *= 4;
-    this.#isUfoLeavingHomeSection = true;
-    return this.#animationPromise;
+  getCurrentOrbitQuarterIndex = (distance) => {
+    if (distance >= 0 && this.#direction === 'RIGHT') return 1;
+    if (distance >= 0 && this.#direction === 'LEFT') return 2;
+    if (distance < 0 && this.#direction === 'LEFT') return 3;
+    return 4;
   };
 
-  stopAnimationIfConditionsFulliled = () => {
-    if (!this.areStopConditionsFullfiled()) return;
-    cancelAnimationFrame(this.#animation);
-    this.#animationPromiseResolve();
-    this.resetAnimation();
-    return true;
+  getScale = () => {
+    let sizeFactor = 0.4;
+    return 1 - this.#depth * sizeFactor;
   };
 
-  areStopConditionsFullfiled = () => {
-    if (!this.#isUfoLeavingHomeSection) return false;
-    let isUfoInFirstEarthCorner = this.#firstEarthCornerX - this.#ufoCenterLeft <= 0;
-    if (!isUfoInFirstEarthCorner) return false;
-    return true;
+  setSpeed = () => {
+    let speedFactor = 0.8;
+    this.#speed = this.#baseSpeed + this.#depth * speedFactor;
   };
 
-  resetAnimation = () => {
-    this.#isUfoLeavingHomeSection = false;
-    this.#speed /= 4;
-    this.#start = undefined;
-    this.#animation = undefined;
-    this.#goToLaunchingPositionIsOn = false;
+  shouldStop = (deltaX) => {
+    if (this.#stop && this.#direction === 'RIGHT' && deltaX > 0 && deltaX <= 2 * this.#speed) return true;
+    return false;
+  };
+
+  stop = () => {
+    cancelAnimationFrame(this.#requestAnimationID);
+    this.reset();
+    this.#resolve();
   };
 }
 
