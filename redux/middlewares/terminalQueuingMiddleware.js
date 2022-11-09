3bd17e65ec8da3f7fa9aa7c8b9eb5d30e7d1ac58
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-09 17:38:40 +0100

    Added queueing middlaware for terminal commands

---
+++ redux/middlewares/terminalQueuingMiddleware.js
@@ -0,0 +1,60 @@
+import types from '../types';
+
+const terminalQueueingMiddleware = (store) => (next) => (action) => {
+  if (action.type !== types.QUEUE_COMMAND) return next(action);
+  const command = createCommandWithPromise(action);
+  if (isQueueEmpty(store)) {
+    addCommandToQueue(next, command);
+    displayCommand(next, command);
+    removeFromQueueWhenTypingFinished(next, command);
+  } else {
+    const lastQueuedCommand = getLastQueuedCommand(store);
+    addCommandToQueue(next, command);
+    displayAfterLastQueuedCommand(next, lastQueuedCommand, command);
+    removeFromQueueWhenTypingFinished(next, command);
+  }
+};
+
+const createCommandWithPromise = (action) => {
+  let resolve;
+  const promise = new Promise((res) => (resolve = res));
+  const command = {
+    text: action.command,
+    promise: promise,
+    resolve: resolve,
+  };
+  return command;
+};
+
+const isQueueEmpty = (store) => {
+  if (store.getState().terminal.queue.length === 0) return true;
+  return false;
+};
+
+const addCommandToQueue = (next, command) => {
+  next({ type: types.QUEUE_COMMAND, command: command });
+};
+
+const displayCommand = (next, command) => {
+  next({ type: types.DISPLAY_COMMAND, command: command });
+};
+
+const removeCommandFromQueue = (next, command) => {
+  next({ type: types.REMOVE_COMMAND_FROM_QUEUE, command: command });
+};
+
+const removeFromQueueWhenTypingFinished = async (next, command) => {
+  await command.promise;
+  removeCommandFromQueue(next, command);
+};
+
+const getLastQueuedCommand = (store) => {
+  return store.getState().terminal.queue.at(-1);
+};
+
+const displayAfterLastQueuedCommand = async (next, lastQueuedCommand, command) => {
+  await lastQueuedCommand.promise;
+  displayCommand(next, command);
+};
+
+export default terminalQueueingMiddleware;
