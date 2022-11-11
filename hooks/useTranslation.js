30de2c13383ff8f899dbecd04a13a30a98e91e32
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-11 02:49:05 +0100

    Fixed handling text translation without available translation

--- hooks/useTranslation.js
+++ hooks/useTranslation.js
@@ -1,25 +1,25 @@
-import translations, {availableTranslations} from '@/translations/translations';
-import {useSelector} from 'react-redux';
+import translations, { availableTranslations } from '@/translations/translations';
+import { useSelector } from 'react-redux';
 
 const useTranslation = (text) => {
-	const language = useSelector(state => state.language);
+  const language = useSelector((state) => state.language);
 
-	const getTranslation = (text, lang) => {
-		return translations[lang][text];
-	}
+  const getTranslation = (text, lang) => {
+    return translations[lang][text];
+  };
 
-	const t = (text) =>{
-		const translatedText = '';
-		availableTranslations.some(translation => {
-			if(translation === language){
-				translatedText = getTranslation(text, language);
-				return true;
-			}
-		})
-		return translatedText;
-	}
+  const t = (text) => {
+    const translatedText = '';
+    availableTranslations.some((translation) => {
+      if (translation === language) {
+        translatedText = getTranslation(text, language);
+        return true;
+      }
+    });
+    return translatedText || text;
+  };
 
-	return {t: t}
-}
+  return { t: t };
+};
 
 export default useTranslation;
