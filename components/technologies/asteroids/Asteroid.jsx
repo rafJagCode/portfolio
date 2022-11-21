fd05aa90437b91aa3167be19e5e19201e1d72bce
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-21 13:51:13 +0100

    Handled explosions in Asteroid component

--- components/technologies/asteroids/Asteroid.jsx
+++ components/technologies/asteroids/Asteroid.jsx
@@ -1,13 +1,16 @@
 import styles from './Asteroid.module.scss';
 import imagesCollisionPoints from './imagesCollisionPoints';
+import Explosion from './explosion/Explosion';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useRef, useState } from 'react';
 
 export default function Asteroid({ asteroidID, imageName, startingPosition }) {
   const asteroidsCollisionPoints = useSelector((state) => state.asteroidsCollisionPoints);
+  const asteroidHits = useSelector((state) => state.asteroidsHits[asteroidID]);
   const dispatch = useDispatch();
   const asteroidRef = useRef(null);
   const [position, setPosition] = useState(startingPosition);
+  const [explosions, setExplosions] = useState([]);
 
   const updateCollisionPoints = () => {
     const asteroidCollisionPoints = imagesCollisionPoints[imageName].map((point) => ({ x: point.x + position.x, y: point.y + position.y }));
@@ -33,8 +36,32 @@ export default function Asteroid({ asteroidID, imageName, startingPosition }) {
     updateCollisionPoints();
   }, [position]);
 
+  const addExplosion = (hitpoint) => {
+    setExplosions((state) => [...state, { id: performance.now() * Math.floor(Math.random() * 1000), hitpoint: hitpoint, size: 50 }]);
+  };
+
+  const removeExplosion = (id) => {
+    setExplosions((explosions) => explosions.filter((explosion) => explosion.id !== id));
+  };
+
+  useEffect(() => {
+    if (!asteroidHits || !asteroidHits.length) return;
+    addExplosion(asteroidHits.at(-1));
+  }, [asteroidHits]);
+
   return (
     <>
+      {explosions.map((explosion) => {
+        return (
+          <Explosion
+            key={explosion.id}
+            explosionID={explosion.id}
+            hitpoint={explosion.hitpoint}
+            size={explosion.size}
+            removeExplosion={removeExplosion}
+          />
+        );
+      })}
       <img
         src={`/static/images/${imageName}.svg`}
         ref={asteroidRef}
