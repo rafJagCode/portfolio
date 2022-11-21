793afe4b4786cd928f8e88df68a5000f651924f6
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-21 13:46:28 +0100

    Added crosshair

---
+++ components/technologies/crosshair/Crosshair.jsx
@@ -0,0 +1,48 @@
+import styles from './Crosshair.module.scss';
+import CrosshairAnimation from './CrosshairAnimation';
+import { refsTypes } from '@/types';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Crosshair() {
+  const dispatch = useDispatch();
+  const crosshairRef = useRef(null);
+  const crosshairAnimationRef = useRef(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const ufoRef = useSelector((state) => state.globalRefs[refsTypes.UFO_REF]);
+  const arrowRight = useSelector((state) => state.keys.ArrowRight);
+  const arrowLeft = useSelector((state) => state.keys.ArrowLeft);
+
+  useEffect(() => {
+    if (!ufoRef || !crosshairRef) return;
+    crosshairAnimationRef.current = new CrosshairAnimation(crosshairRef, ufoRef, setPosition);
+    crosshairAnimationRef.current.start();
+    return () => {
+      crosshairAnimationRef.current.stop();
+    };
+  }, [ufoRef, crosshairRef]);
+
+  useEffect(() => {
+    if (!crosshairAnimationRef) return;
+    crosshairAnimationRef.current.setIsRightArrowPressed(arrowRight.pressed);
+  }, [arrowRight, crosshairAnimationRef]);
+
+  useEffect(() => {
+    if (!crosshairAnimationRef) return;
+    crosshairAnimationRef.current.setIsLeftArrowPressed(arrowLeft.pressed);
+  }, [arrowLeft, crosshairAnimationRef]);
+
+  useEffect(() => {
+    if (!crosshairAnimationRef) return;
+    dispatch({ type: 'SET_CROSSHAIR_ANGLE', angle: crosshairAnimationRef.current.getCurrentAngle() });
+  }, [position, crosshairAnimationRef]);
+
+  return (
+    <img
+      ref={crosshairRef}
+      className={styles.crosshair}
+      src={'/static/images/crosshair.svg'}
+      style={{ left: position.x, top: position.y }}
+    ></img>
+  );
+}
---
+++ components/technologies/crosshair/CrosshairAnimation.js
@@ -0,0 +1,80 @@
+class CrosshairAnimation {
+  #r = 100;
+  #x = -100;
+  #y = 0;
+  #angle = Math.PI;
+  #angleVelocity = Math.PI / 180;
+  #ufoCenter = { x: 0, y: 0 };
+  #crosshairPosition = { x: 0, y: 0 };
+  #crosshair;
+  #ufo;
+  #setPosition;
+  #requestAnimationID;
+  #isRightArrowPressed = false;
+  #isLeftArrowPressed = false;
+
+  constructor(crosshairRef, ufoRef, setPosition) {
+    this.#crosshair = crosshairRef.current;
+    this.#ufo = ufoRef.current;
+    this.#setPosition = setPosition;
+    this.step = this.step.bind(this);
+  }
+
+  start() {
+    if (this.#requestAnimationID) return;
+    requestAnimationFrame(this.step);
+  }
+
+  stop() {
+    if (!this.#requestAnimationID) return;
+    cancelAnimationFrame(this.step);
+  }
+
+  step() {
+    this.setUfoCenter();
+    if (this.#isRightArrowPressed) this.handleRightArrow();
+    if (this.#isLeftArrowPressed) this.handleLeftArrow();
+    this.moveCrosshairCenterTo(this.#ufoCenter.x + this.#x, this.#ufoCenter.y - this.#y);
+    this.#requestAnimationID = requestAnimationFrame(this.step);
+  }
+
+  handleRightArrow() {
+    this.#angle -= this.#angleVelocity;
+    this.#x = this.#r * Math.cos(this.#angle);
+    this.#y = this.#r * Math.sin(this.#angle);
+  }
+  handleLeftArrow() {
+    this.#angle += this.#angleVelocity;
+    this.#x = this.#r * Math.cos(this.#angle);
+    this.#y = this.#r * Math.sin(this.#angle);
+  }
+
+  moveCrosshairCenterTo(x, y) {
+    const positionX = x - this.#crosshair.offsetWidth / 2;
+    const positionY = y - this.#crosshair.offsetHeight / 2;
+    if (this.#crosshairPosition.x !== positionX || this.#crosshairPosition.y !== positionY) {
+      this.#crosshairPosition = { x: positionX, y: positionY };
+      this.#setPosition(this.#crosshairPosition);
+    }
+  }
+
+  setIsRightArrowPressed(pressed) {
+    this.#isRightArrowPressed = pressed;
+  }
+
+  setIsLeftArrowPressed(pressed) {
+    this.#isLeftArrowPressed = pressed;
+  }
+
+  setUfoCenter() {
+    const ufoCenterX = this.#ufo.offsetLeft + this.#ufo.offsetWidth / 2;
+    const ufoCenterY = this.#ufo.offsetTop + this.#ufo.offsetHeight / 2;
+    this.#ufoCenter = { x: ufoCenterX, y: ufoCenterY };
+  }
+
+  getCurrentAngle() {
+    return this.#angle;
+  }
+}
+
+export default CrosshairAnimation;
