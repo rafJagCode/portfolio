153108249a852d1ba7edf7486b56dd2cb438f938
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-21 13:50:04 +0100

    Started working on game controller

---
+++ components/technologies/game_controller/GameController.jsx
@@ -0,0 +1,39 @@
+import styles from './GameController.module.scss';
+import availableKeys from './availableKeys';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useCallback } from 'react';
+
+export default function GameController() {
+  const dispatch = useDispatch();
+  const gameState = useSelector((state) => state.gameState);
+
+  const handleKeyStateChange = useCallback((e) => {
+    if (!availableKeys.includes(e.key)) return;
+    if (e.repeat) return;
+    dispatch({ type: 'UPDATE_KEY_STATE', key: e.key });
+  }, []);
+
+  useEffect(() => {
+    if (gameState === 'STARTED') {
+      fullpage_api.setAllowScrolling(false);
+      fullpage_api.setKeyboardScrolling(false);
+      addEventListener('keydown', handleKeyStateChange);
+      addEventListener('keyup', handleKeyStateChange);
+      return;
+    }
+    if (gameState === 'FINISHED') {
+      fullpage_api.setAllowScrolling(true);
+      fullpage_api.setKeyboardScrolling(true);
+    }
+    removeEventListener('keydown', handleKeyStateChange);
+    removeEventListener('keyup', handleKeyStateChange);
+  }, [gameState]);
+
+  return (
+    <div className={styles.game_controller}>
+      {gameState !== 'STARTED' && <button onClick={() => dispatch({ type: 'SET_GAME_STATE', gameState: 'STARTED' })}>START GAME</button>}
+      {gameState !== 'FINISHED' && <button onClick={() => dispatch({ type: 'SET_GAME_STATE', gameState: 'FINISHED' })}>END GAME</button>}
+      {gameState === 'STARTED' && <button onClick={() => dispatch({ type: 'SET_GAME_STATE', gameState: 'PAUSED' })}>PAUSE</button>}
+    </div>
+  );
+}
---
+++ components/technologies/game_controller/availableKeys.js
@@ -0,0 +1,27 @@
+// const keys = {
+//   w: {
+//     pressed: false,
+//   },
+//   a: {
+//     pressed: false,
+//   },
+//   s: {
+//     pressed: false,
+//   },
+//   d: {
+//     pressed: false,
+//   },
+//   arrowRight: {
+//     pressed: false,
+//   },
+//   arrowLeft: {
+//     pressed: false,
+//   },
+//   space: {
+//     pressed: false,
+//   },
+// };
+
+const availableKeys = ['ArrowRight', 'ArrowLeft', ' '];
+
+export default availableKeys;
