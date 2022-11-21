6268a025939fceffe125bd89c615f6fed43bb0ab
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-21 19:20:27 +0100

    Added debounced hiding asteroid healthbar

--- components/technologies/asteroid/healthbar/Healthbar.jsx
+++ components/technologies/asteroid/healthbar/Healthbar.jsx
@@ -1,8 +1,30 @@
 import styles from './Healthbar.module.scss';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Healthbar({ startingHealthPoints, healthPoints }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const timerIDRef = useRef(null);
+
+  const debounceHideHealthbar = (delay) => {
+    if (timerIDRef && timerIDRef.current) {
+      clearTimeout(timerIDRef.current);
+    }
+    timerIDRef.current = setTimeout(() => {
+      setIsVisible(false);
+    }, delay);
+  };
+
+  useEffect(() => {
+    if (healthPoints === startingHealthPoints) return;
+    setIsVisible(true);
+    debounceHideHealthbar(3000);
+  }, [healthPoints]);
+
   return (
-    <div className={styles.healthbar}>
+    <div
+      className={styles.healthbar}
+      style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+    >
       <div
         className={styles.healthbar__state}
         style={{ width: `${(healthPoints / startingHealthPoints) * 100}%` }}
