d81d9152531a9ef8da85e51c5178ba4e084a1fb7
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-21 13:52:08 +0100

    Handled showing and hiding crosshair

--- components/technologies/Technologies.jsx
+++ components/technologies/Technologies.jsx
@@ -1,11 +1,17 @@
 import styles from './Technologies.module.scss';
+import GameController from './game_controller/GameController';
+import Crosshair from './crosshair/Crosshair';
 import Lasers from './lasers/Lasers';
 import Asteroid from './asteroids/Asteroid';
 import asteroids from './asteroids/asteroidsStartingSetup';
+import { useSelector } from 'react-redux';
 
 export default function Technologies() {
+  const gameState = useSelector((state) => state.gameState);
   return (
     <div className={styles.technologies}>
+      {gameState !== 'FINISHED' && <Crosshair />}
+      <GameController />
       <Lasers />
       {asteroids.map((asteroid) => {
         return (
