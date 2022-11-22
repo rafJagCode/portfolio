a3482094f2d51c49f1921756a975433358e81af9
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-22 03:54:12 +0100

    Added redux-devtools

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
 import terminalQueueingMiddleware from './middlewares/terminalQueuingMiddleware';
-import { combineReducers } from 'redux';
-import reducers from './reducers';
 import gameReducers from './gameReducers';
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 
-const store = createStore(combineReducers({ ...reducers, ...gameReducers }), applyMiddleware(terminalQueueingMiddleware));
+const store = createStore(combineReducers({ ...reducers, ...gameReducers }), composeWithDevTools(applyMiddleware(terminalQueueingMiddleware)));
 
 export default store;
