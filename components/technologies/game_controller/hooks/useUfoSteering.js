a0b4112505039f6a51662d50047609b46e049329
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-11-22 04:24:06 +0100

    Added returning to launching position after game end

--- components/technologies/game_controller/hooks/useUfoSteering.js
+++ components/technologies/game_controller/hooks/useUfoSteering.js
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 const useUfoSteering = () => {
   const gameState = useSelector((state) => state.gameState);
   const holdLaunchingPositionAnimation = useSelector((state) => state.animations[animationsTypes.HOLD_LAUNCHING_POSITION_ANIMATION]);
+  const flyToLaunchingPositionAnimation = useSelector((state) => state.animations[animationsTypes.FLY_TO_LAUNCHING_POSITION_ANIMATION]);
   const ufoSteeringAnimation = useSelector((state) => state.animations[animationsTypes.UFO_STEERING_ANIMATION]);
   const keys = useSelector((state) => state.keys);
 
@@ -17,14 +18,20 @@ const useUfoSteering = () => {
     ufoSteeringAnimation.stopAnimation();
   };
 
+  const flyToLaunchingPosition = async () => {
+    await flyToLaunchingPositionAnimation.startAnimation();
+    holdLaunchingPositionAnimation.startAnimation();
+  };
+
   useEffect(() => {
-    if (!holdLaunchingPositionAnimation || !ufoSteeringAnimation) return;
+    if (!holdLaunchingPositionAnimation || !ufoSteeringAnimation || !flyToLaunchingPositionAnimation) return;
     if (gameState === 'STARTED') {
       enableUfoSteering();
       return;
     }
+    if (gameState === 'FINISHED') flyToLaunchingPosition();
     disableUfoSteering();
-  }, [gameState, holdLaunchingPositionAnimation, ufoSteeringAnimation]);
+  }, [gameState, holdLaunchingPositionAnimation, ufoSteeringAnimation, flyToLaunchingPositionAnimation]);
 
   useEffect(() => {
     if (!ufoSteeringAnimation) return;
