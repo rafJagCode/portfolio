bd79f1be3514cb44bf1f078d23eba4d6f0ac4f98
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-12-05 14:51:13 +0100

    Moved AsteroidClass to asteroid utils folder

---
+++ components/technologies/asteroid/utils/AsteroidClass.js
@@ -0,0 +1,59 @@
+import uuid from 'react-uuid';
+
+class Asteroid {
+  asteroidID;
+  imageName;
+  startingPosition;
+  asteroidKind;
+  asteroidSize;
+  startingHealthPoints;
+  startingSpeed;
+
+  constructor(imageName, startingPosition) {
+    this.asteroidID = uuid();
+    this.imageName = imageName;
+    this.startingPosition = startingPosition;
+    const asteroidKind = this.getAsteroidKind(imageName);
+    this.setKindDependentProperties(asteroidKind);
+  }
+
+  getAsteroidKind(imageName) {
+    const [prefix, asteroidKind, number] = imageName.split('-');
+    return asteroidKind;
+  }
+
+  setKindDependentProperties(asteroidKind) {
+    let asteroidSize;
+    let startingHealthPoints;
+    let startingSpeed;
+    switch (asteroidKind) {
+      case 'bit': {
+        asteroidSize = 20;
+        startingHealthPoints = 1;
+        startingSpeed = 1;
+        break;
+      }
+      case 'fragment': {
+        asteroidSize = 40;
+        startingHealthPoints = 2;
+        startingSpeed = 2;
+        break;
+      }
+      case 'whole': {
+        asteroidSize = 100;
+        startingHealthPoints = 5;
+        startingSpeed = 1;
+        break;
+      }
+      default: {
+        asteroidSize = 100;
+      }
+    }
+    this.asteroidKind = asteroidKind;
+    this.asteroidSize = asteroidSize;
+    this.startingHealthPoints = startingHealthPoints;
+    this.startingSpeed = startingSpeed;
+  }
+}
+
+export default Asteroid;
