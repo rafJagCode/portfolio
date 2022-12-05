e17e43f0379f2b85958f6d2d84c34f633a355cad
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-12-05 14:50:39 +0100

    Changed configuration files name convention

---
+++ configuration/asteroids_setup.js
@@ -0,0 +1,13 @@
+import Asteroid from '@/components/technologies/asteroid/utils/AsteroidClass';
+
+const asteroids = [
+  new Asteroid('asteroid-whole-1', { x: 900, y: 200 }),
+  //   new Asteroid('asteroid-whole-2', { x: 100, y: 400 }),
+  //   new Asteroid('asteroid-whole-3', { x: 600, y: 100 }),
+  //   new Asteroid('asteroid-fragment-1', { x: 100, y: 800 }),
+  //   new Asteroid('asteroid-fragment-2', { x: 200, y: 800 }),
+  //   new Asteroid('asteroid-bit-1', { x: 600, y: 700 }),
+  //   new Asteroid('asteroid-bit-2', { x: 700, y: 700 }),
+];
+
+export default asteroids;
