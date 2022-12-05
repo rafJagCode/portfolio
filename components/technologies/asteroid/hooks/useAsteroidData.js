1f8f29c0021364ac0848b20e09b142b9b2084faa
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2022-12-05 14:52:29 +0100

    Returned asteroidData from useAsteroidData hook

--- components/technologies/asteroid/hooks/useAsteroidData.js
+++ components/technologies/asteroid/hooks/useAsteroidData.js
@@ -1,23 +1,24 @@
-import imagesCollisionData from '@/configuration/imagesCollisionData';
+import imagesCollisionData from '@/configuration/images_collision_data';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
 const useAsteroidData = (asteroidID, imageName, position) => {
   const dispatch = useDispatch();
+  const asteroidData = imagesCollisionData[imageName];
 
   const cleanAsteroidData = () => {
     dispatch({ type: 'CLEAN_ASTEROID_DATA', asteroidID });
   };
 
   useEffect(() => {
-    dispatch({ type: 'UPDATE_ASTEROID_DATA', asteroidID, asteroidData: imagesCollisionData[imageName] });
+    dispatch({ type: 'UPDATE_ASTEROID_DATA', asteroidID, asteroidData });
   }, []);
 
   useEffect(() => {
     dispatch({ type: 'UPDATE_ASTEROID_POSITION', asteroidID, posX: position.x, posY: position.y });
   }, [position]);
 
-  return cleanAsteroidData;
+  return [asteroidData, cleanAsteroidData];
 };
 
 export default useAsteroidData;
