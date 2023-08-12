30fcad0764adcfa63cc1710aadd6a9cc3206d784
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-13 00:37:03 +0200

    Downloaded fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html className='dark'>
-        <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={true} />
-          <link href='https://fonts.googleapis.com/css2?family=Fruktur&family=Saira:wght@300;500;800&family=VT323&display=swap' rel='stylesheet' />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
