ef458679cc004240e1dc15f19efe5bdfdb70d414
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-17 13:31:16 +0200

    Changed starting asteroids positions to %

--- configuration/asteroids_setup_conf.js
+++ configuration/asteroids_setup_conf.js
@@ -1,5 +1,5 @@
 import Asteroid from '@/components/sections/technologies/asteroid/utils/AsteroidClass';
 
-const asteroids = [new Asteroid('asteroid-whole-1', { x: 900, y: 200 }, 'git'), new Asteroid('asteroid-whole-2', { x: 100, y: 400 }, 'react')];
+const asteroids = [new Asteroid('asteroid-whole-1', { x: '50%', y: '25%' }, 'git'), new Asteroid('asteroid-whole-2', { x: '5%', y: '50%' }, 'react')];
 
 export default asteroids;
