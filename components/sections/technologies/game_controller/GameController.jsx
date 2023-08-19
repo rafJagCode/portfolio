fc89cf564cb351a7fb1e23940e1cbca319953571
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-19 17:01:23 +0200

    Moved handling win and lose of the game to game controller

--- components/sections/technologies/game_controller/GameController.jsx
+++ components/sections/technologies/game_controller/GameController.jsx
@@ -3,7 +3,7 @@ import Instructions from '../instructions/Instructions';
 import GameMenu from './game_menu/GameMenu';
 import availableKeys from '@/configuration/available_keys_conf';
 import useUfoSteering from './hooks/useUfoSteering';
-import { gameStates, compareGameState } from 'redux/game/gameStateMachine';
+import { gameStates, gameActions, compareGameState } from 'redux/game/gameStateMachine';
 import actions from 'redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useCallback } from 'react';
@@ -11,7 +11,23 @@ import { useEffect, useCallback } from 'react';
 export default function GameController() {
   const dispatch = useDispatch();
   const gameState = useSelector((state) => state.gameState);
+  const asteroids = useSelector((state) => state.asteroids);
+  const ufoLives = useSelector((state) => state.ufoLives);
   useUfoSteering();
+
+  const getFullLives = () => {
+    return ufoLives.reduce((acc, curr) => (curr === 'full' ? acc + 1 : acc), 0);
+  };
+
+  useEffect(() => {
+    if (!compareGameState(gameState, gameStates.PLAYING)) return;
+    if (!getFullLives()) dispatch(actions.updateGameState(gameActions.LOSE_GAME));
+  }, [ufoLives]);
+
+  useEffect(() => {
+    if (!compareGameState(gameState, gameStates.PLAYING)) return;
+    if (!asteroids.length) dispatch(actions.updateGameState(gameActions.WIN_GAME));
+  }, [asteroids]);
 
   const handleKeyStateChange = useCallback((e) => {
     if (!availableKeys.includes(e.key)) return;
@@ -39,6 +55,7 @@ export default function GameController() {
       dispatch(actions.clearUfoHits());
       dispatch(actions.clearTechnologies());
       dispatch(actions.reloadAmmunition());
+      dispatch(actions.resetLives());
     }
     removeEventListener('keydown', handleKeyStateChange);
     removeEventListener('keyup', handleKeyStateChange);
--- components/sections/technologies/hooks/useAsteroids.js
+++ components/sections/technologies/hooks/useAsteroids.js
@@ -1,16 +1,9 @@
 import actions from 'redux/actions';
-import { gameStates, gameActions, compareGameState } from 'redux/game/gameStateMachine';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 
 const useAsteroids = () => {
   const asteroids = useSelector((state) => state.asteroids);
-  const gameState = useSelector((state) => state.gameState);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (compareGameState(gameState, gameStates.PLAYING) && !asteroids.length) dispatch(actions.updateGameState(gameActions.WIN_GAME));
-  }, [asteroids]);
 
   const addAsteroid = (asteroid) => {
     dispatch(actions.addAsteroid(asteroid));
