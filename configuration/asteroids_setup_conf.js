425d4d794eb09dbcfb91365cc4ee76f82e308c86
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-21 11:08:00 +0200

    Changed asteroid starting setup

--- configuration/asteroids_setup_conf.js
+++ configuration/asteroids_setup_conf.js
@@ -4,7 +4,7 @@ const asteroids = [
   new Asteroid('asteroid-whole-1', { x: '50%', y: '25%' }, ['html', 'javascript']),
   new Asteroid('asteroid-whole-2', { x: '5%', y: '50%' }, ['react', 'vue']),
   new Asteroid('asteroid-whole-3', { x: '40%', y: '70%' }, ['git', 'github']),
-  new Asteroid('asteroid-whole-4', { x: '70%', y: '70%' }, ['npm', 'webpack']),
+  new Asteroid('asteroid-whole-4', { x: '10%', y: '10%' }, ['npm', 'webpack']),
   new Asteroid('asteroid-whole-5', { x: '25%', y: '25%' }, ['css', 'sass']),
 ];
 
