ac3f18ca312d79bb370231a04143c6ac8f064d71
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-23 18:56:32 +0200

    Cleared _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,18 +3,9 @@ import 'scss/global.scss';
 import Head from 'next/head';
 import store from '../redux/store';
 import { Provider } from 'react-redux';
-import { useEffect, useState } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-  const [pageLoading, setPageLoading] = useState(true);
-
-  useEffect(() => {
-    setPageLoading(false);
-  }, []);
-
-  return pageLoading ? (
-    <div>Loading</div>
-  ) : (
+  return (
     <Provider store={store}>
       <Head>
         <title>Portfolio</title>
