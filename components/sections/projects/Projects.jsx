14d76f6d452dcbe83465e896241c15e5da521609
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-05-23 22:02:00 +0200

    Added fourth project

--- components/sections/projects/Projects.jsx
+++ components/sections/projects/Projects.jsx
@@ -15,6 +15,7 @@ export default function Projects() {
           <Cow imageName='cow1' tabIndex='8' />
           <Cow imageName='cow2' tabIndex='9' />
           <Cow imageName='cow3' tabIndex='10' />
+          <Cow imageName='cow4' tabIndex='11' />
         </div>
       </div>
     </div>
--- components/sections/projects/terminal/project_links/ProjectLinks.jsx
+++ components/sections/projects/terminal/project_links/ProjectLinks.jsx
@@ -6,11 +6,11 @@ import { BiLinkExternal } from 'react-icons/bi';
 export default function ProjectLinks({ project }) {
   return (
     <div className={styles.container}>
-      <a className={styles.link} target='_blank' href={links[project].github} tabIndex='11'>
+      <a className={styles.link} target='_blank' href={links[project].github} tabIndex='12'>
         <ImGithub />
         GITHUB
       </a>
-      <a className={styles.link} target='_blank' href={links[project].live} tabIndex='12'>
+      <a className={styles.link} target='_blank' href={links[project].live} tabIndex='13'>
         <BiLinkExternal />
         LIVE
       </a>
