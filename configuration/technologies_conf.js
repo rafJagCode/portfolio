f61093b9ab34b4716256fe03fd269da4a05a0ed4
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-05-30 02:01:26 +0200

    Changed technologies icons

--- configuration/technologies_conf.js
+++ configuration/technologies_conf.js
@@ -1,3 +1,44 @@
-const knownTechnologies = ['html', 'javascript', 'css', 'sass', 'react', 'vue', 'git', 'github', 'npm', 'webpack'];
+const knownTechnologies = [
+  {
+    name: 'html',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+  },
+  {
+    name: 'javascript',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  {
+    name: 'css',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+  },
+  {
+    name: 'react',
+    link: 'https://reactjs.org/',
+  },
+  {
+    name: 'vue',
+    link: 'https://vuejs.org/',
+  },
+  {
+    name: 'laravel',
+    link: 'https://laravel.com/',
+  },
+  {
+    name: 'symfony',
+    link: 'https://symfony.com/',
+  },
+  {
+    name: 'nodejs',
+    link: 'https://nodejs.org/',
+  },
+  {
+    name: 'mysql',
+    link: 'https://www.mysql.com/',
+  },
+  {
+    name: 'git',
+    link: 'https://git-scm.com/',
+  },
+];
 
 export default knownTechnologies;
