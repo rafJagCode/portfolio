304d0db8a67aa5c09655f417bbf23996b6ef16f5
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-07 11:42:12 +0200

    Changed title and content meta tag for google snippets

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,7 +8,8 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
-        <title>Portfolio</title>
+        <title>Rafał Jagielski</title>
+        <meta name='description' content='Rafał Jagielski - Web Developer specializing in Vue.js and React. Explore my portfolio to see my latest projects, skills, and experience in creating dynamic and responsive web applications.' />
       </Head>
       <Component {...pageProps} />
     </Provider>
