01f4f7ce294cceca4c348ba22943922bfbcb6b46
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-19 21:22:50 +0200

    Remove touch device detection criterion

--- components/sections/technologies/game_controller/hooks/useTouchDetection.js
+++ components/sections/technologies/game_controller/hooks/useTouchDetection.js
@@ -5,22 +5,17 @@ const useTouchDetection = () => {
 
   useEffect(() => {
     let isMobile = false;
-    if (navigator !== null || navigator !== undefined) {
-    }
-    if ('maxTouchPoints' in navigator) {
-      isMobile = navigator.maxTouchPoints > 0;
-    } else if ('msMaxTouchPoints' in navigator) {
+    if ('msMaxTouchPoints' in navigator) {
       if (navigator !== null) {
-        isMobile = navigator?.msMaxTouchPoints > 0;
+        isMobile = navigator.msMaxTouchPoints > 0;
       }
     } else {
       const mQ = window.matchMedia && matchMedia('(pointer:coarse)');
       if (mQ && mQ.media === '(pointer:coarse)') {
         isMobile = !!mQ.matches;
       } else if ('orientation' in window) {
-        isMobile = true; // deprecated, but good fallback
+        isMobile = true;
       } else {
-        // Only as a last resort, fall back to user agent sniffing
         const UA = navigator?.userAgent;
         isMobile = /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
       }
