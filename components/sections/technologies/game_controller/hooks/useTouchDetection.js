01049486e298ad8f265c64b8b81dc72d247cf957
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-19 21:07:20 +0200

    Improved touch detection hook

--- components/sections/technologies/game_controller/hooks/useTouchDetection.js
+++ components/sections/technologies/game_controller/hooks/useTouchDetection.js
@@ -4,12 +4,29 @@ const useTouchDetection = () => {
   const [isTouchDevice, setIsTouchDevice] = useState(false);
 
   useEffect(() => {
-    let result = false;
-    if ('ontouchstart' in window || navigator.msMaxTouchPoints || navigator.maxTouchPoints) {
-      result = true;
+    let isMobile = false;
+    if (navigator !== null || navigator !== undefined) {
+    }
+    if ('maxTouchPoints' in navigator) {
+      isMobile = navigator.maxTouchPoints > 0;
+    } else if ('msMaxTouchPoints' in navigator) {
+      if (navigator !== null) {
+        isMobile = navigator?.msMaxTouchPoints > 0;
+      }
+    } else {
+      const mQ = window.matchMedia && matchMedia('(pointer:coarse)');
+      if (mQ && mQ.media === '(pointer:coarse)') {
+        isMobile = !!mQ.matches;
+      } else if ('orientation' in window) {
+        isMobile = true; // deprecated, but good fallback
+      } else {
+        // Only as a last resort, fall back to user agent sniffing
+        const UA = navigator?.userAgent;
+        isMobile = /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+      }
     }
 
-    setIsTouchDevice(result);
+    setIsTouchDevice(isMobile);
   }, []);
   return isTouchDevice;
 };
